dea79881aa578c172b2044418151b87f2d878f04
HongJiMin <42535969+3StarAnchovy@users.noreply.github.com>
2021-03-26 15:24:48 +0900

    api test2

--- json_test.py
+++ json_test.py
@@ -1,8 +1,20 @@
 #요건 내일
+
 import requests
+class RiotData:
+    def __init__(self,userID): #set
+        self.userID=userID
+        self.api_key = "RGAPI-cce887fb-67e8-487a-a70d-2476970599c2"
+        self.url="https://kr.api.riotgames.com"
+    def getUserData(self): #유저 정보
+        url = self.url+"/lol/summoner/v4/summoners/by-name/"+self.userID+"?api_key="+self.api_key
+        res = requests.get(url)
+        return res
 
-api_key = "RGAPI-cce887fb-67e8-487a-a70d-2476970599c2"
-url ="https://kr.api.riotgames.com"+"/lol/summoner/v4/summoners/by-name/"+"HongJiMin"+"?api_key="+api_key
-res = requests.get(url)
+userID = input("userID : ")
+test = RiotData(userID)
+print(test.getUserData().json())
 
-print(res.json())
+#api_key = "RGAPI-cce887fb-67e8-487a-a70d-2476970599c2"
+#url ="https://kr.api.riotgames.com"+"/lol/summoner/v4/summoners/by-name/"+"HongJiMin"+"?api_key="+api_key
+#res = requests.get(url)
