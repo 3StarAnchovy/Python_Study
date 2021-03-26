f403d4c4f67ea4c02fc4e91b6b7d49c9bc70f9c8
HongJiMin <42535969+3StarAnchovy@users.noreply.github.com>
2021-03-26 14:56:36 +0900

    api test

--- json_test.py
+++ json_test.py
@@ -1,1 +1,8 @@
 #요건 내일
+import requests
+
+api_key = "RGAPI-cce887fb-67e8-487a-a70d-2476970599c2"
+url ="https://kr.api.riotgames.com"+"/lol/summoner/v4/summoners/by-name/"+"HongJiMin"+"?api_key="+api_key
+res = requests.get(url)
+
+print(res.json())
